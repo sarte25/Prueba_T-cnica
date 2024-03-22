1de5553d07dbbb7740d3cdf0ea0aa4442ef6f782
sarte25 <sebasmena.121@gmail.com>
2024-03-21 22:34:08 -0500

    segund subida de los archivos en los cuales se agrego diferentes estilos tanto como para los contendeores como el fondo ademas se añadio la parte de funcionalidad de javascript faltando en ella que la descripcion se muestre igual que la tarea ademas de que me permita editar la tarea una vez creada y se añadio codigo html

---
+++ src/javascript/scritp.js
@@ -0,0 +1,121 @@
+const fecha = document.querySelector('#fecha')
+const lista = document.querySelector('#lista')
+const elemento = document.querySelector('#elemento')
+const input = document.querySelector('#input')
+const botonEnter = document.querySelector('#boton-enter')
+const check = 'fa-check-circle'
+const uncheck = 'fa-circle'
+const lineThrough = 'line-through'
+let LIST
+let id = 0;// para que inicielice en 0 cada tarea tendra un id diferente
+
+//creacion de la fecha actualizada 
+
+const FECHA = new Date ()
+fecha.innerHTML = FECHA.toLocaleDateString('es-MX',{weekday: 'long', month: 'short', day:'numeric'})
+
+// funcion de agregar tarea 
+
+function agregarTarea( tarea,id,realizado,eliminado) {
+    if(eliminado) {return} 
+
+    const REALIZADO = realizado ? check : uncheck
+    const LINE = realizado ? lineThrough : '' 
+
+    const elemento = `
+                        <li id="elemento">
+                        <i class="far ${REALIZADO}" data="realizado" id="${id}"></i>
+                        <p class="text ${LINE}">${tarea}</p>
+                        <i class="fas fa-trash de" data="eliminado" id="${id}"></i> 
+                        </li>
+                    `
+    lista.insertAdjacentHTML("beforeend",elemento)
+
+}
+
+
+// funcion de Tarea Realizada 
+
+function tareaRealizada(element) {
+    element.classList.toggle(check)
+    element.classList.toggle(uncheck)
+    element.parentNode.querySelector('.text').classList.toggle(lineThrough)
+    LIST[element.id].realizado = LIST[element.id].realizado ?false :true //Si
+}
+
+function tareaEliminada(element){
+    element.parentNode.parentNode.removeChild(element.parentNode)
+    LIST[element.id].eliminado = true
+    console.log(LIST)
+}
+
+
+// se crea un evento para escuchar el enter y para habilitar el boton 
+
+botonEnter.addEventListener('click', ()=> {
+    const tarea = input.value
+    if(tarea){
+        agregarTarea(tarea,id,false,false)
+        LIST.push({
+            nombre : tarea,
+            id : id,
+            realizado : false,
+            eliminado : false
+        })
+        localStorage.setItem('TODO',JSON.stringify(LIST))
+        id++
+        input.value = ''
+    }
+
+})
+
+document.addEventListener('keyup', function (event) {
+    if (event.key=='Enter'){
+        const tarea = input.value
+        if(tarea) {
+            agregarTarea(tarea,id,false,false)
+        LIST.push({
+            nombre : tarea,
+            id : id,
+            realizado : false,
+            eliminado : false
+        })
+        localStorage.setItem('TODO',JSON.stringify(LIST))
+     
+        input.value = ''
+        id++
+        console.log(LIST)
+        }
+    }
+    
+})
+
+
+lista.addEventListener('click',function(event){
+    const element = event.target 
+    const elementData = element.attributes.data.value
+    console.log(elementData)
+    
+    if(elementData == 'realizado') {
+        tareaRealizada(element)
+    }
+    else if(elementData == 'eliminado') {
+        tareaEliminada(element)
+        console.log("elimnado")
+    }
+    localStorage.setItem('TODO',JSON.stringify(LIST))
+})
+
+
+let data = localStorage.getItem('TODO')
+if(data){
+    LIST = JSON.parse(data)
+    console.log(LIST)
+    id = LIST.length
+    cargarLista(LIST)
+}else {
+    LIST = []
+    id = 0
+}
+
+
